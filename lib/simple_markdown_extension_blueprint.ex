281489332b1cb4f51fae069b57847917a266b8d7
Stefan Johnson <sj@scrimpycat.io>
2017-11-30 22:20:24 +1100

    Create rule

--- lib/simple_markdown_extension_blueprint.ex
+++ lib/simple_markdown_extension_blueprint.ex
@@ -1,2 +1,17 @@
 defmodule SimpleMarkdownExtensionBlueprint do
+    def rule() do
+        {
+            :blueprint,
+            %{
+                match: ~r/\A@blueprint[\[\(](.*?)[\]\)]/,
+                capture: 0,
+                option: fn input, [_, { index, length }] ->
+                    case String.split(binary_part(input, index, length), " ", strip: true) do
+                        ["plot", graph|args] -> { Module.safe_concat(Mix.Tasks.Blueprint.Plot, String.to_atom(String.capitalize(graph))), :run, [args] }
+                    end
+                end,
+                rules: []
+            }
+        }
+    end
 end
