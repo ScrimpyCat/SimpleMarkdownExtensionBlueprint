8491da48f719b9634e49c6a376158c7b8a57543b
Stefan Johnson <sj@scrimpycat.io>
2017-12-05 17:16:15 +1100

    Get ready to publish to hex

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule SimpleMarkdownExtensionBlueprint.Mixfile do
     def project do
         [
             app: :simple_markdown_extension_blueprint,
+            description: "An extension for SimpleMarkdown to introduce some markup to create and embed a blueprint graph.",
             version: "0.1.0",
             elixir: "~> 1.5",
             start_permanent: Mix.env == :prod,
-            deps: deps()
+            deps: deps(),
+            package: package()
         ]
     end
 
@@ -19,9 +21,17 @@ defmodule SimpleMarkdownExtensionBlueprint.Mixfile do
     # Run "mix help deps" to learn about dependencies.
     defp deps do
         [
-            { :simple_markdown, "~> 0.3.0" },
-            { :blueprint, "~> 0.2.0" },
+            { :simple_markdown, "~> 0.3" },
+            { :blueprint, "~> 0.2" },
             { :ex_doc, "~> 0.18", only: :dev }
+        ]
+    end
+
+    defp package do
+        [
+            maintainers: ["Stefan Johnson"],
+            licenses: ["BSD 2-Clause"],
+            links: %{ "GitHub" => "https://github.com/ScrimpyCat/SimpleMarkdownExtensionBlueprint" }
         ]
     end
 end
