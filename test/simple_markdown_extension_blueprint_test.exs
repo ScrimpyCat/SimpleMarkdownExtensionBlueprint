8e6e3f048f7fc3c2e30ccac9d0a0e077b291d004
Stefan Johnson <sj@scrimpycat.io>
2017-12-09 21:41:06 +1100

    Test parsing

--- test/simple_markdown_extension_blueprint_test.exs
+++ test/simple_markdown_extension_blueprint_test.exs
@@ -1,4 +1,26 @@
 defmodule SimpleMarkdownExtensionBlueprintTest do
     use ExUnit.Case
     doctest SimpleMarkdownExtensionBlueprint
+
+    test "matching blueprint command" do
+        assert [{ :blueprint, ["@blueprint[plot app]"], { Mix.Tasks.Blueprint.Plot.App, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint[plot app]", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint[plot mod]"], { Mix.Tasks.Blueprint.Plot.Mod, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint[plot mod]", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint[plot fun]"], { Mix.Tasks.Blueprint.Plot.Fun, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint[plot fun]", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint[plot msg]"], { Mix.Tasks.Blueprint.Plot.Msg, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint[plot msg]", [SimpleMarkdownExtensionBlueprint.rule])
+
+        assert [{ :blueprint, ["@blueprint[plot app --colour --path test]"], { Mix.Tasks.Blueprint.Plot.App, :run, [["--colour", "--path", "test"]] } }] == SimpleMarkdown.Parser.parse("@blueprint[plot app --colour --path test]", [SimpleMarkdownExtensionBlueprint.rule])
+
+        assert [{ :blueprint, ["@blueprint(plot app)"], { Mix.Tasks.Blueprint.Plot.App, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint(plot app)", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint(plot mod)"], { Mix.Tasks.Blueprint.Plot.Mod, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint(plot mod)", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint(plot fun)"], { Mix.Tasks.Blueprint.Plot.Fun, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint(plot fun)", [SimpleMarkdownExtensionBlueprint.rule])
+        assert [{ :blueprint, ["@blueprint(plot msg)"], { Mix.Tasks.Blueprint.Plot.Msg, :run, [[]] } }] == SimpleMarkdown.Parser.parse("@blueprint(plot msg)", [SimpleMarkdownExtensionBlueprint.rule])
+
+        assert [{ :blueprint, ["@blueprint(plot app --colour --path test)"], { Mix.Tasks.Blueprint.Plot.App, :run, [["--colour", "--path", "test"]] } }] == SimpleMarkdown.Parser.parse("@blueprint(plot app --colour --path test)", [SimpleMarkdownExtensionBlueprint.rule])
+    end
+
+    test "invalid blueprint command" do
+        assert_raise CaseClauseError, fn -> SimpleMarkdown.Parser.parse("@blueprint[]", [SimpleMarkdownExtensionBlueprint.rule]) end
+        assert_raise CaseClauseError, fn -> SimpleMarkdown.Parser.parse("@blueprint[plot]", [SimpleMarkdownExtensionBlueprint.rule]) end
+        assert_raise ArgumentError, fn -> SimpleMarkdown.Parser.parse("@blueprint[plot test]", [SimpleMarkdownExtensionBlueprint.rule]) end
+    end
 end
