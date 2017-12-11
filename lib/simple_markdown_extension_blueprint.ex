34cc8b375a24e590ba53f734af76ff345d756549
Stefan Johnson <sj@scrimpycat.io>
2017-12-11 21:22:04 +1100

    Add styling options

--- lib/simple_markdown_extension_blueprint.ex
+++ lib/simple_markdown_extension_blueprint.ex
@@ -5,7 +5,28 @@ defmodule SimpleMarkdownExtensionBlueprint do
 
       The command takes the form of `@blueprint[]` or `@blueprint()`.
       Where inside the brackets are the arguments that can be
-      passed to a `blueprint` escript.
+      passed to a `blueprint` escript. The `@blueprint` prefix may
+      optionally be followed by options separated by a `-` to
+      customize how it should be added to the page.
+
+      These options are:
+
+      * Overriding the width of the element that comes after it,
+      by providing a `w` before the literal to be used for the
+      width (where no literal means it will use the default
+      pixel width). e.g. If it comes before the `[]` it will
+      affect the width of the SVG element, whereas if it comes
+      before an `embed` option it will affect the width of the
+      container.
+      * Overriding the height of the element that comes after it,
+      by providing a `h` before the literal to be used for the
+      height (where no literal means it will use the default
+      pixel height). e.g. If it comes before the `[]` it will
+      affect the height of the SVG element, whereas if it comes
+      before an `embed` option it will affect the height of the
+      container.
+      * Place the SVG in a scrollable container, by providing
+      the `embed` option.
 
       ## Example
 
@@ -14,9 +35,41 @@ defmodule SimpleMarkdownExtensionBlueprint do
       Which produces:
 
       @blueprint[plot app --messages --colour]
+
+      ## Fixed Size Example
+
+        @blueprint-w300px-h50px[plot app --messages --colour]
+
+      Which produces:
+
+      @blueprint-w300px-h50px[plot app --messages --colour]
+
+      ## Relative Size Example
+
+        @blueprint-w50%-h50%[plot app --messages --colour]
+
+      Which produces:
+
+      @blueprint-w50%-h50%[plot app --messages --colour]
+
+      ## Embed Example
+
+        @blueprint-embed[plot app --messages --colour]
+
+      Which produces:
+
+      @blueprint-embed[plot app --messages --colour]
+
+      ## Fixed Size Embed With Relative Size Example
+
+        @blueprint-w100px-h50px-embed-w500%-h500%[plot app --messages --colour]
+
+      Which produces:
+
+      @blueprint-w300px-h150px-embed-w1000%-h1000%[plot app --messages --colour]
     """
 
-    defstruct [command: nil, width: "100%", height: "100%"]
+    defstruct [command: nil, width: "100%", height: "100%", embed: false, embed_width: "100%", embed_height: "100%"]
 
     @doc """
       The rule for matching blueprint commands.
@@ -37,6 +90,7 @@ defmodule SimpleMarkdownExtensionBlueprint do
                     |> Enum.reduce(opt, fn
                         "w" <> width, opt -> %{ opt | width: width }
                         "h" <> height, opt -> %{ opt | height: height }
+                        "embed", opt -> %SimpleMarkdownExtensionBlueprint{ command: opt.command, embed: true, embed_width: opt.width, embed_height: opt.height }
                     end)
                 end,
                 rules: []
@@ -60,12 +114,16 @@ defmodule SimpleMarkdownExtensionBlueprint do
             File.rm!(name)
 
             String.replace(svg, ~r/\A(.|\n)*?(?=<svg)/m, "", global: false)
+            |> String.trim()
             |> set_attribute("width", opts.width)
             |> set_attribute("height", opts.height)
-            |> String.trim()
+            |> set_view(opts.embed, opts.embed_width, opts.embed_height)
         end
 
         defp set_attribute(svg, _, ""), do: svg
         defp set_attribute(svg, attr, value), do: String.replace(svg, ~r/\A(.*?)#{attr}=".*?"/, "\\1#{attr}=\"#{value}\"", global: false)
+
+        defp set_view(svg, false, width, height), do: svg
+        defp set_view(svg, true, width, height), do: "<iframe srcdoc='#{svg}' width='#{width}' height='#{height}'></iframe>"
     end
 end
